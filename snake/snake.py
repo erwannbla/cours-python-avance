8ff9f4a1d9f9dc4f946d78a53df21d260144f7c4
Erwann Blamengin <erwann.blamenginr@mines-paristech.fr>
2022-11-08 17:01:59 +0100

    damier resolu mais pb sur score->pas de memoire

--- snake/snake.py
+++ snake/snake.py
@@ -12,10 +12,16 @@ parser = argparse.ArgumentParser(description='Process some integers.')
  #                   help='sum the integers (default: find the max)')
 parser.add_argument('--height', type=int, help='height', default=400) #le joueur definit largeur et longueur de sa fenetre de jeu +pixel
 parser.add_argument('--width', type=int, help='width', default=400)
-parser.add_argument('--heightpixel', type=int, help='heightp', default=20)
-parser.add_argument('--widthpixel', type=int, help='widthp', default=20)
+parser.add_argument('--dimpixel', type=int, help='dimp', default=20)
+
 args = parser.parse_args()
 print(args) #affiche dans le bash les arguments
+
+if (args.height % args.dimpixel !=0) or (args.width % args.dimpixel !=0) :
+    print('erreur:largeur pixel nest pas un diviseur de la largeur de la fenetre')
+    exit()
+else:
+    pass
 
 pg.init()
 screen=pg.display.set_mode((args.width,args.height)) #on definit un ecran
@@ -24,29 +30,29 @@ clock=pg.time.Clock() #on definit une horloge
 running=True  #condition pour que la boucle tourne
 
 score=0
-def damier(x,y,xp,yp):
-        for i in range(x//xp):
-            for j in range(y//yp):
-                if (i+j)%2 ==0:
-                    rect = pg.Rect(i*y, j*x, xp, yp)
+def damier(x,y,p):
+        for i in range(x//p):
+            for j in range(y//p):
+                if (i+j)%2 !=0:
+                    rect = pg.Rect(i*p, j*p, p, p)
 
                 # appel à la méthode draw.rect()
-                    color = [0, 0, 0] # couleur blanche
+                    color = [0, 0, 0] # couleur noire
                     pg.draw.rect(screen,color , rect)
                 else:
-                    rect = pg.Rect(i*y, j*x, xp, yp)
+                    rect = pg.Rect(i*p, j*p, p, p)
 
                 # appel à la méthode draw.rect()
-                    color = [255, 255, 255] # noir
+                    color = [255, 255, 255] # blanc
                     pg.draw.rect(screen,color,rect)
 
 def snake(L):   #code de l'affichage du serpent
     for rectangle in L:  #L liste des tuples de positionnement des rectangles vert
-        rectang=pg.Rect(rectangle[0]*args.widthpixel,rectangle[1]*args.heightpixel,args.widthpixel,args.heightpixel)
+        rectang=pg.Rect(rectangle[0]*args.dimpixel,rectangle[1]*args.dimpixel,args.dimpixel,args.dimpixel)
         pg.draw.rect(screen,(0,254,0),rectang)
 
 def fruit(a,b):   #code de l'affichage du fruit
-    rec=pg.Rect(a*args.widthpixel,b*args.heightpixel,args.widthpixel,args.heightpixel)
+    rec=pg.Rect(a*args.dimpixel,b*args.dimpixel,args.dimpixel,args.dimpixel)
     pg.draw.rect(screen,(254,0,0),rec)
     
 L= [(9, 13),
@@ -55,8 +61,8 @@ L= [(9, 13),
     (12,13)
 ]
 direction=(0,1)
-a=np.random.randint(0,args.width//args.widthpixel)
-b=np.random.randint(0,args.height//args.heightpixel)
+a=np.random.randint(0,args.width//args.dimpixel)
+b=np.random.randint(0,args.height//args.dimpixel)
 
 while running:
 
@@ -79,22 +85,22 @@ while running:
                     direction=(1,0)
 
    # random_color=(randint(0,255),randint(0,255),randint(0,255))
-    screen.fill((255,255,255))
-    pg.display.update()
-    damier(args.widthpixel,args.heightpixel,args.widthpixel,args.heightpixel)
-    pg.display.update()
+    #screen.fill((255,255,255))
+  
+    damier(args.width,args.height,args.dimpixel)
+    #pg.display.update()
     L.append((L[-1][0]+direction[0],L[-1][1]+direction[1])) #on fait avancer le snake en rajoutant un rectangle a sa liste et enlevant le dernier
     fruit(a,b)
     if L[-1]!=(a,b):  #si la tete ne rencontre pas le fruit
         del L[0]        #on supprime la derniere case ie le snake ne grandit pas
     else:
-        a=np.random.randint(0,args.height//args.heightpixel)    #sinon on genere nouveau fruit
-        b=np.random.randint(0,args.height//args.heightpixel)
+        a=np.random.randint(0,args.height//args.dimpixel)    #sinon on genere nouveau fruit
+        b=np.random.randint(0,args.height//args.dimpixel)
         score=score+1
     
-    font = pg.font.Font(None, 20)
-    text = font.render(str(score),0, (255,255,255))
-    screen.blit(text, (20,20))
+    #font = pg.font.Font(None, 20)
+    #text = font.render(str(score),0, (255,255,255))
+    #screen.blit(text, (20,20))
     
     snake(L)
     pg.display.update()
@@ -102,10 +108,10 @@ while running:
     if L[-1] in L[:-1]:     #si la tete du snake touche son corps on perd
         running=False
         print('game over')
-    elif L[-1][0]<0 or L[-1][0]>= (args.width//args.widthpixel):
+    elif L[-1][0]<0 or L[-1][0]>= (args.width//args.dimpixel):
         running=False
         print('game over')
-    elif L[-1][1]<0 or L[-1][1]>= (args.height//args.heightpixel):
+    elif L[-1][1]<0 or L[-1][1]>= (args.height//args.dimpixel):
         running=False
         print('game over')
     
@@ -115,16 +121,17 @@ if not(os.path.exists('highscore.txt')):
     with open('highscore.txt','w') as f:
         Name=input('Nom du joueur:')
         tup=((score,Name))
-        f.write('\n'.join(f'{score} {Name}'))
+        f.write(''.join(f'{score} {Name}'))
 else:
     with open('highscore.txt','r') as f:
         for count, line in enumerate(f):
             pass
         if count+1<5:
-            with open('highscore.txt','r') as f:
+            with open('highscore.txt','w') as f:
                 Name=input('Nom du joueur:')
                 tup=((score,Name))
-                f.write('\n'.join(f'{tup[0]} {tup[1]}'))
+                #f.write('\n')
+                f.write(''.join(f'{tup[0]} {tup[1]}'))
         else:
             lines = f.readlines()
             L=[]
